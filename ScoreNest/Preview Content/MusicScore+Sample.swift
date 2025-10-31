09905f2a948474bb1780dac1a3049fea52256a6d
YujiaWang <wyj11427@163.com>
2025-10-31 18:01:54 +0800

    refact: 简单修改了Preview数据的添加逻辑

--- ScoreNest/Preview Content/MusicScore+Sample.swift
+++ ScoreNest/Preview Content/MusicScore+Sample.swift
@@ -1,23 +1,24 @@
 extension MusicScore {
+    static let projectBaseRoot = "/Users/wangyujia/Programming/XCodeProject/ScoreNest/"
     
     /// Provides an array of sample `MusicScore` instances for preview purposes.
     static var sampleScores: [MusicScore] {
         let score1 = MusicScore(title: "Canon in D", pages: [
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/1.png", pageNumber: 1),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/2.png", pageNumber: 2),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/3.png", pageNumber: 3),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/4.png", pageNumber: 4),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/5.png", pageNumber: 5),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/6.png", pageNumber: 6),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/7.png", pageNumber: 7),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/8.png", pageNumber: 8),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/1.png", pageNumber: 1),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/2.png", pageNumber: 2),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/3.png", pageNumber: 3),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/4.png", pageNumber: 4),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/5.png", pageNumber: 5),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/6.png", pageNumber: 6),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/7.png", pageNumber: 7),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/8.png", pageNumber: 8),
         ])
         
         let score2 = MusicScore(title: "River Flows in You", pages: [
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/4.png", pageNumber: 1),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/5.png", pageNumber: 2),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/6.png", pageNumber: 3),
-            ScorePage(imageFileName: "/Users/wangyujia/Programming/XCodeProject/ScoreNest/ImagesTest/7.png", pageNumber: 4)
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/4.png", pageNumber: 1),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/5.png", pageNumber: 2),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/6.png", pageNumber: 3),
+            ScorePage(imageFileName: projectBaseRoot + "ImagesTest/7.png", pageNumber: 4)
         ])
         
         return [score1, score2]
